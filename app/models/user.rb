1a4e807716a8b558670edf8f58c4c4b349bac545
njonge9 <frednjonge9@gmail.com>
2023-06-08 14:49:26 +0300

    Create a html form with no rails helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
     validates :password, length: {minimum: 6}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, length: { maximum: 255}, format: {with: VALID_EMAIL_REGEX}
-    validates :name, length: { maximum: 50}
+    validates :username, length: { maximum: 50}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :users only: [:new, :create]
+  get 'users/new'
+  get 'users/create'
+  resources :users, only: [:new, :create]
 end
