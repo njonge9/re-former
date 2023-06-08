4d92a897ef0ff00be64a2b778076d26d06e66a1b
njonge9 <frednjonge9@gmail.com>
2023-06-08 15:07:28 +0300

    Code for create method in controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :email,:username,:password presence: true
+    validates :email,:username,:password, presence: true
     validates :password, length: {minimum: 6}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, length: { maximum: 255}, format: {with: VALID_EMAIL_REGEX}
