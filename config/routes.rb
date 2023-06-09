b050a3c1a9bed19e8fb3bdd952158bb310b4640e
njonge9 <frednjonge9@gmail.com>
2023-06-09 03:02:24 +0300

    final touches

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'users/new'
   get 'users/create'
-  get 'users/edit/:id'
-  resources :users, only: [:new, :create,:edit]
+  # get 'users/edit/:id'
+  resources :users, only: [:new, :create,:edit, :update]
 end
